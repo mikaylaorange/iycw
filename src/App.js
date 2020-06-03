bc1e389a27ba3bb2401b01cdda0bf33b15067949
Darryl Brooks <darryl.brooks@myfuelmaster.com>
2020-06-03 19:32:24 -0400

    Refactored Code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from "react";
 import "./App.css";
 import States from "./Components/States";
-import { TextField, GridList, Typography } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
+import { TextField, GridList, makeStyles } from "@material-ui/core";
 
 const mockData = require("./mockData.json");
 
@@ -17,8 +16,8 @@ function App() {
   const classes = useStyles();
   return (
     <div className="App">
-      <Typography variant="h4">States</Typography>
-      <TextField type="search" className="searchTextField" variant="outlined" />
+      <h1 className="headline">States</h1>
+      <TextField type="search" variant="outlined" />
       <GridList cellHeight={180} rows={2} cols={3} className={classes.gridList}>
         {mockData.map((states, key) => (
           <States state={states.state} bailfunds={states.bail_funds} />
--- src/Components/States.js
+++ src/Components/States.js
@@ -1,11 +1,5 @@
 import React, { useState } from "react";
-import {
-  GridListTile,
-  GridListTileBar,
-  Collapse,
-  CardContent,
-} from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
+import { GridListTile, GridListTileBar, Collapse, CardContent, makeStyles } from "@material-ui/core";
 import { ExpandMore } from "@material-ui/icons";
 import clsx from "clsx";
 import "./States.css";
