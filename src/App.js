d64083ebb757403a0ec2ef2152a7c348beb2091b
Darryl Brooks <darryl.brooks@myfuelmaster.com>
2020-06-03 18:32:02 -0400

    Created State Component Still Needs Testing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import States from "./Components/States";
+import { TextField, GridList, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const mockData = require("./mockData.json");
+
+const useStyles = makeStyles((theme) => ({
+  gridList: {
+    width: "100%",
+    justifyContent: "center",
+  },
+}));
 
 function App() {
+  const classes = useStyles();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Typography variant="h4">States</Typography>
+      <TextField type="search" className="searchTextField" variant="outlined" />
+      <GridList cellHeight={180} rows={2} cols={3} className={classes.gridList}>
+        {mockData.map((states, key) => (
+          <States state={states.state} bailfunds={states.bail_funds} />
+        ))}
+      </GridList>
     </div>
   );
 }
