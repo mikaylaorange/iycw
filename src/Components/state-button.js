1ae01589d4a7fc45e38e8cf44f4d8834f6a1427d
Darryl Brooks <darryl.brooks@myfuelmaster.com>
2020-06-15 18:06:37 -0400

    Finished animations for mobile/desktop and fixed some styling issues

--- src/Components/state-button.js
+++ src/Components/state-button.js
@@ -9,64 +9,107 @@ class State extends React.Component {
     this.state = {
       stateId: null,
       isStatesPage: true,
-    }
+    };
   }
-  onClick = e => {
-    e.preventDefault();
-    this.setState(
-      {
-        stateId: this.props.stateName,
-        isStatesPage: !this.state.isStatesPage,
-      },
-      () => console.log(this.state.stateId)
+
+  cardAnimation = () => {
+    const donation = document.getElementById(
+      `donation = ${this.props.stateName}`
     );
+    const state = document.getElementById(`state = ${this.props.stateName}`);
+
+    if (!this.state.isStatesPage) {
+      // Closing animation / Fading animation
+      if (window.innerWidth > 850) {
+        // Desktop Animation
+        donation.style.opacity = "0";
+        donation.style.left = "0%";
+        donation.style.visibility = "hidden";
+        state.style.left = "0%";
+      } else {
+        // Mobile Animation
+        donation.style.opacity = "0";
+        donation.style.visibility = "hidden";
+        state.style.opacity = "1";
+        state.style.visibility = "visible";
+      }
+    } else {
+      // Opening / Appearing animation
+      if (window.innerWidth > 850) {
+        // Desktop Animation
+        donation.style.opacity = "1";
+        donation.style.left = "184px";
+        donation.style.visibility = "visible";
+        state.style.left = "-184px";
+      } else {
+        // Mobile Animation
+        donation.style.opacity = "1";
+        donation.style.visibility = "visible";
+        state.style.opacity = "0";
+        state.style.visibility = "hidden";
+      }
+    }
+    this.setState({
+      stateId: this.props.stateName,
+      isStatesPage: !this.state.isStatesPage,
+    });
+  };
+
+  onClick = (e) => {
+    e.preventDefault();
+    this.cardAnimation();
   };
 
   render() {
-    const donationsPage = (
-      <div>
-        <div id={this.props.stateName} className="state-div">
-        <div className="view-wrapper">
-          <p style={{ color: "#EADEDA" }}>{this.props.stateName}</p>
-          <Button
-            onClick={this.onClick} className="view" style={{ 
-              border: "2px solid #EADEDA", 
-              borderRadius: "15px",
-              color: "#EADEDA"  }}>
-                {this.state.isStatesPage ? "View" : "Hide"}
-          </Button>
-        </div>
-      </div>
-      <div className="donations-div">
-      {this.props.funds.map((fund, key) => (
-          <Donations key={key}
-            name={fund.name}
-            url={fund.url}
-          />
-        )
-        
-        )}
-        </div>
-      </div>
-    )
     const statesPage = (
       <div id={this.props.stateName} className="state-div">
         <div className="view-wrapper">
           <p style={{ color: "#EADEDA" }}>{this.props.stateName}</p>
           <Button
-            onClick={this.onClick} className="view" style={{ 
-              border: "2px solid #EADEDA", 
+            onClick={this.onClick}
+            className="view"
+            style={{
+              border: "2px solid #EADEDA",
               borderRadius: "15px",
-              color: "#EADEDA"  }}>
-                {this.state.isStatesPage ? "View" : "Hide"}
+              color: "#EADEDA",
+            }}
+          >
+            {this.state.isStatesPage ? "View" : "Hide"}
           </Button>
         </div>
       </div>
-    )
-    return (this.state.isStatesPage ? statesPage : donationsPage)
+    );
 
+    return (
+      <div className="state-wrapper">
+        <div id={`state = ${this.props.stateName}`} className="state-page-div">
+          {statesPage}
+        </div>
+        <div
+          className="donation-wrapper"
+          id={`donation = ${this.props.stateName}`}
+        >
+          <div className="donations-div">
+            {this.props.funds.map((fund, key) => (
+              <Donations key={key} name={fund.name} url={fund.url} />
+            ))}
+            <Button
+              onClick={this.onClick}
+              className="donation-view"
+              style={{
+                margin: "10% 0% 0% 0%",
+                border: "2px solid #EADEDA",
+                borderRadius: "15px",
+                color: "#EADEDA",
+              }}
+            >
+              {this.state.isStatesPage ? "View" : "Hide"}
+            </Button>
+          </div>
+        </div>
+      </div>
+    );
   }
-
 }
 
 export default State;
--- src/Components/states-page.js
+++ src/Components/states-page.js
@@ -1,20 +1,13 @@
-import firestoreRef from '../firebase';
+import firestoreRef from "../firebase";
 import React, { useEffect, useState } from "react";
 import "../App.css";
-
-import { Route, Switch, useLocation } from 'react-router-dom';
-
 import States from "./state-button";
 import { TextField } from "@material-ui/core";
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles({
   root: {
-    backgroundColor: "transparent",
-    width: "510px",
-    height: "66px",
     border: "2px solid #EADEDA",
-    borderRadius: "15px",
   },
   input: {
     fontSize: 30,
@@ -22,22 +15,20 @@ const useStyles = makeStyles({
     top: "300%",
     textAlign: "center",
     marginTop: "2%",
-  }
-})
+  },
+});
 //TODO: Add fonts to use for the site and clean this page up.
 function StatesPage() {
   const classes = useStyles();
   const [statesList, setStatesList] = useState([]);
-  /** useEffect to retrieve all state names from firestore. The state name is the collection ID, 
+  /** useEffect to retrieve all state names from firestore. The state name is the collection ID,
    * so that will be passed down to the StateButton component. This is because we determine which
    * state is clicked within that component.
    */
   useEffect(() => {
     let statesArr = [];
     async function getStates() {
-      let querySnapshot = await firestoreRef
-        .collection('bail-funds')
-        .get();
+      let querySnapshot = await firestoreRef.collection("bail-funds").get();
       querySnapshot.forEach(function (doc) {
         if (doc.data().funds !== undefined) {
           statesArr.push([doc.id, doc.data().abbreviation, doc.data().funds]);
@@ -53,31 +44,47 @@ function StatesPage() {
     for (let i = 0; i < statesList.length; i++) {
       if (statesList[i][0].toUpperCase().indexOf(input) > -1) {
         document.getElementById(`${statesList[i][0]}`).style.display = "";
+        document.getElementById(
+          `donation = ${statesList[i][0]}`
+        ).style.display = "";
       } else {
         document.getElementById(`${statesList[i][0]}`).style.display = "none";
+        document.getElementById(
+          `donation = ${statesList[i][0]}`
+        ).style.display = "none";
       }
     }
-  }
+  };
 
   return (
     <div className="App">
       <div className="App-header">
         <h1 className="headline">If You Can't Walk</h1>
-        <TextField className={classes.root} placeholder="Search for a State" type="search" id="input" onChange={search} InputProps={{
-          classes: {
-            input: classes.input,
-          },
-          disableUnderline: true
-        }} />
-        <div className="states-list">
+        <TextField
+          className="textfield"
+          classes={{
+            root: classes.root,
+          }}
+          placeholder="Search for a State"
+          type="search"
+          id="input"
+          onChange={search}
+          InputProps={{
+            classes: {
+              input: classes.input,
+            },
+            disableUnderline: true,
+          }}
+        />
+        <div>
           {statesList.map((states, key) => (
-            <States key={key}
+            <States
+              key={key}
               stateName={states[0]}
               abbreviation={states[1]}
               funds={states[2]}
             />
-          )
-          )}
+          ))}
         </div>
       </div>
     </div>
